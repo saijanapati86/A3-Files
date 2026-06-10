1760825e6cf0c04fb6214ed2fad674d8ec7ea1e4
Sai Charan Janapati <janapatisaicharan2005@gmail.com>
2026-06-10 22:00:27 +1000

    Added js changes to the checkout page, now able to click on payment option before proceeding with payment

---
+++ checkout.js
@@ -0,0 +1,44 @@
+// Selecting Payment Option
+
+let selectedPaymentMethod = null;
+
+const paymentButtons =
+    document.querySelectorAll(".payment_option");
+
+        paymentButtons.forEach((button) => {
+
+            button.addEventListener("click", () => {
+
+                paymentButtons.forEach((btn) => {
+                    btn.classList.remove("active");
+                });
+
+                button.classList.add("active");
+
+                selectedPaymentMethod = button.textContent.trim();
+
+                document.getElementById("paymentError").textContent = "";
+
+            });
+
+        });
+
+
+        const payButton =
+            document.querySelector(".payment_btn");
+
+        payButton.addEventListener("click", (e) => {
+
+            e.preventDefault();
+
+            if (!selectedPaymentMethod) {
+                document.getElementById("paymentError").textContent =
+                    "Please Select a Payment Method*";
+                return;
+
+            }
+
+            alert("Proceeding With: " +  selectedPaymentMethod);
+        });
+
+        
